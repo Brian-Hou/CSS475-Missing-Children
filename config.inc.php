c51e19228859e1c5bbf384ddd74306c0d7104c6c
bhou <bhou@uw.edu>
2019-05-26 10:00:48 -0700

    Adding some starter files

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User:
+ */
+$servername = 'vergil.u.washington.edu'; // Your Server (if noot)
+$username = 'root';
+$password = 'CSS475';
+$database = 'CSS 475 Missing Children';
+$port = 42000; // Your Port
