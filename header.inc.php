d1aa56daf49faf5912d551a265b3975199ce221a
Brian Hou <brianghou@gmail.com>
2019-05-30 15:42:16 -0700

    Adding a couple more files...

--- header.inc.php
+++ header.inc.php
@@ -1,1 +1,16 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User:
+ * Date: 7/24/2018
+ * Time: 3:37 PM
+ */
+?>
+<!-- This is a common navigation base to call different web pages on the website -->
+<div class="topnav">
+    <a href="index.php">Home</a>
+    <a href="list_missing_children.php">List Missing Children</a>
+<!--    <a href="list_products.php">List Products</a>-->
+</div>
+<h1>Sample PHP Database Program</h1>
+
